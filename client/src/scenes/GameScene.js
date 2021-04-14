f088ba024efa3296e58cbea70e69695ebb20a62c
Matthew Cook <matthewcook13987@gmail.com>
2021-04-14 17:46:35 -0500

    adds test object to test enemy player movement

--- client/src/scenes/GameScene.js
+++ client/src/scenes/GameScene.js
@@ -1,6 +1,7 @@
 import {Scene} from 'phaser';
 import io from 'socket.io-client';
 import Player from '../entities/Player';
+import TestObject from '../entities/TestObject'
 import unbreakableBlock from '../assets/platform.png';
 
 
@@ -16,6 +17,11 @@ export default class GameScene extends Scene {
     this.socket = this.registry.get('socket');
     this.state = this.registry.get('state');
     const player1 = this.createPlayer();
+    this.socket.on('playerMoved', function (data) {
+      console.log("others players movement data:", data);
+      const player2 = createOtherPlayer();
+    })
+    const player2 = this.createOtherPlayer(this, data.x, data.y)
     player1.setTexture('tankRight');
     const map = this.createMap();
     const layers = this.createLayers(map);
@@ -24,99 +30,7 @@ export default class GameScene extends Scene {
     // player1.projectilesGroup.addCollider(layers.wallLayer, player1.projectilesGroup.killAndHide);
     // this.physics.add.collider(player1.projectilesGroup, layers.wallLayer);
 
-
-
-    //SOCKETS
-    // let self = this;
-    // this.socket = io('http://localhost:3000') //this will need to change on prod server
-    // this.socket.on('connect', function() {
-    //   console.log(`User: ... has connected`);
-    // });
-    this.socket.on('playerMoved', function (data) {
-      console.log("others players movement data:", data);
-    })
-
-    
-
-    // this.socket.emit('test', "hello from GameScene");
-
-    // this.socket.on('currentPlayers', (players) => {
-    //   Object.keys(players).forEach((id) => {
-    //     if (players[id].playerId === self.socket.id) {
-    //       addPlayer(self, players[id]);
-    //     }
-    //   });
-    // });
-    // this.otherPlayers = this.physics.add.group();
-    // this.socket.on('currentPlayers', function (players) {
-    //   Object.keys(players).forEach(function (id) {
-    //     if (players[id].playerId === self.socket.id) {
-    //       addPlayer(self, players[id]);
-    //     } else {
-    //       addOtherPlayers(self, players[id]);
-    //     }
-    //   });
-    // });
-
-    // this.socket.on('newPlayer', function (playerInfo) {
-    //   addOtherPlayers(self, playerInfo);
-    // });
-
-    // this.socket.on('remove', function (playerId) {
-    //   self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-    //     if (playerId === otherPlayer.playerId) {
-    //       otherPlayer.destroy();
-    //     }
-    //   });
-    // });
-
-    // // Add players
-    // function addPlayer(self, playerInfo) {
-    //   self.tankP1 = self.physics.add.image(playerInfo.x, playerInfo.y, 'tankP1').setOrigin(0.5, 0.5).setDisplaySize(64, 64);
-    //   // if (playerInfo.team === 'blue') {
-    //   //   self.tankP1.setTint(0x0000ff);
-    //   // } else {
-    //   //   self.tankP1.setTint(0xff0000);
-    //   // }
-    //   self.tankP1.setDrag(100);
-    //   self.tankP1.setAngularDrag(100);
-    //   self.tankP1.setMaxVelocity(200);
-    // }
-
-    // function addOtherPlayers(self, playerInfo) {
-    //   const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'tankP2').setOrigin(0.5, 0.5).setDisplaySize(64, 64);
-    //   // if (playerInfo.team === 'blue') {
-    //   //   otherPlayer.setTint(0x0000ff);
-    //   // } else {
-    //   //   otherPlayer.setTint(0xff0000);
-    //   // }
-    //   otherPlayer.playerId = playerInfo.playerId;
-    //   self.otherPlayers.add(otherPlayer);
-    // }
-
-  //   // Input
-    
-
-  //   this.socket.on('playerMoved', function (playerInfo) {
-  //     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-  //       if (playerInfo.playerId === otherPlayer.playerId) {
-  //         otherPlayer.setRotation(playerInfo.rotation);
-  //         otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-  //       }
-  //     });
-  //   });
-   
-
-  //   spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-  //   // Collisions
-  //   this.physics.add.collider(tankP1, unbreakable);
-  //   this.physics.add.overlap(p1Bullets, unbreakable, destroyBullet, null, this);
-
-    // bullets = this.physics.add.group();
-
-  }
-
+  } 
   
   createMap() {
     const map = this.make.tilemap({key: 'map1'});
@@ -143,6 +57,7 @@ export default class GameScene extends Scene {
   }
   
   createOtherPlayer() {
-    
+    return new TestObject(this, x, y);
   }
+  
 }
