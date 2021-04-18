69921753d861d078e2e75e3beec025e8f6056cc7
Eric Murray <ermurray31@gmail.com>
2021-04-17 21:13:59 -0600

    rebuilt file structure to work with webpack and heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors   = require('cors');
 const http   = require('http').createServer(app);
 const morgan = require('morgan');
 const ORIGIN = process.env.ORIGIN || 'http://localhost:8080'
-const PORT = proces.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 app.use(cors());
 
 app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
